39a6ab88a994f44bbede2dea36dd1adb3db72ace
CPenalba <carolinapenalbacorpa@gmail.com>
2025-02-10 10:02:25 +0100

    Add project files.

---
+++ NetCoreLinqToSqlInjection/Controllers/CochesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreLinqToSqlInjection.Models;
+
+namespace NetCoreLinqToSqlInjection.Controllers
+{
+    public class CochesController : Controller
+    {
+        private Coche car;
+
+        public CochesController()
+        {
+            this.car = new Coche();
+        }
+
+        public IActionResult Index()
+        {
+            return View(this.car);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string accion)
+        {
+            if (accion.ToLower() == "acelerar")
+            {
+                this.car.Acelerar();
+            } else
+            {
+                this.car.Frenar();
+            }
+            return View(this.car);
+        }
+    }
+}
---
+++ NetCoreLinqToSqlInjection/Models/Coche.cs
@@ -0,0 +1,38 @@
+﻿namespace NetCoreLinqToSqlInjection.Models
+{
+    public class Coche
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Imagen { get; set; }
+        public int Velocidad { get; set; }
+        public int VelocidadMaxima { get; set; }
+
+        public Coche()
+        {
+            this.Marca = "Cupra";
+            this.Modelo = "Formetor";
+            this.Imagen = "cupra.jpg";
+            this.Velocidad = 0;
+            this.VelocidadMaxima = 120;
+        }
+
+        public void Acelerar()
+        {
+            this.Velocidad += 10;
+            if (this.Velocidad >= this.VelocidadMaxima)
+            {
+                this.Velocidad = this.VelocidadMaxima;
+            }
+        }
+
+        public void Frenar()
+        {
+            this.Velocidad -= 10;
+            if (this.Velocidad < 0)
+            {
+                this.Velocidad = 0;
+            }
+        }
+    }
+}
